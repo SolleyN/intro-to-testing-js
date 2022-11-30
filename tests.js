30105f9a8d91dd58d0656ed9cf5615ba91d5e79c
Nickolas Solley <nicksolley@gmail.com>
2022-11-30 15:28:55 -0600

    working on testing

--- tests.js
+++ tests.js
@@ -16,6 +16,14 @@ describe('helloWorld', function() {
         it('should be a defined function', function () {
             expect(typeof sayHello).toBe('function');
         });
+        it('should return a string when called', function() {
+            expect(typeof sayHello()).toBe("string");
+        });
+            it('should return the string "say hello" when executed', function() {
+                expect(sayHello()).toBe("say hello");
+            });
+});
+        });
 
-    });})
+
 
